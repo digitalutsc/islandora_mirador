ad5137b644a68db23361df740d1374b98753acd5
AndrewEffendi <skz@ZenBookDuo.localdomain>
2022-04-13 09:55:30 -0400

    minor tweak

--- js/mirador_viewer.js
+++ js/mirador_viewer.js
@@ -20,32 +20,16 @@
   var base;
 
   function init(context, settings) {
-    /*
-      function loadScript(url, callback) {
-        // Adding the script tag to the head as suggested before
-        var head = document.head;
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = url;
-  
-        // Then bind the event to the callback function.
-        // There are several events for cross browser compatibility.
-        script.onreadystatechange = callback;
-        script.onload = callback;
-  
-        // Fire the loading
-        head.appendChild(script);
-      }
-      */
-    //loadScript(javascriptURL, function (javascriptURL) {
     if (!initialized) {
       initialized = true;
-      const endpointUrl = "http://localhost:8888/annotation";
+      //const endpointUrl = "http://localhost:8888/annotation";
       const config = {
+        /*
         annotation: {
           adapter: (canvasId) =>
             new SimpleAnnotationServerV2Adapter(canvasId, endpointUrl),
         },
+        */
 
         id: base,
         manifests: {
